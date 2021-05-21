73eea53ef0c883bab15d91344999cd7601caec7a
maggie3000 <72889935+maggie3000@users.noreply.github.com>
2021-05-20 17:02:35 -0700

    submit

--- main.py
+++ main.py
@@ -14,45 +14,22 @@ response = requests.request("GET", url)
 responseJsonObj = json.loads(response.text)
 
 #appending dictionary items into a list
-showList1 = []
+libraryList = []
 for data in responseJsonObj['tv_shows']:
-    showList1.append(data['name'])
-n = 1
+    libraryList.append(data['name'])
 
-#creating a class
-class Shows:
+showList =[]
+
+def ShowRecommendations(series):
+    #validation
+    if series < 0 or series > 20:
+        raise ValueError("Number must be between 0 and 20")
     #initialization
-    def __init__(self, series):
-        #validation
-        if series < 0 or series > 20:
-            raise ValueError("Series must be between 0 and 20")
-        self._series = series
-        self._list = []
-        self._dict = {}
-        self._dictID = 0
-        self.show_series()
-
-    #function to build a random data list and dictionary
-    def show_series(self):
-        limit = self._series
-        #starting array/list
-        f = [random.sample((showList1), k=self.series)]
-        self._list.append(f[0])
-        self._dict[self._dictID] = self._list.copy()
-        self._dictID += 1
-
-    #getters enable class variables to be called outside of class
-    @property
-    def series(self):
-        return self._series
-    @property
-    def list(self):
-        return self._list
-    @property
-    def number(self):
-        return self._list[self._dictID - 1]
-    def get_sequence(self, nth):
-        return self._dict[nth]
+    showList = []
+    #build a random data list
+    f = [random.sample((libraryList), k=series)]
+    showList.append(f[0])
+    return showList
 
 #routing
 @app.route('/')
@@ -60,10 +37,10 @@ class Shows:
 def minilab():
     if request.method == 'POST':
         n = int(request.form.get("shows"))
-        return render_template("minilab.html", showrecs=Shows(n))
+        return render_template("minilab.html", showrecs=ShowRecommendations(n), inputvalue=n)
     else:
         #default is 1
-        return render_template("minilab.html", showrecs=Shows(1))
+        return render_template("minilab.html", showrecs=ShowRecommendations(1), inputvalue=1)
 
 #runs the application
 if __name__ == "__main__":
